2fbfb4603a78d68ac4d57882c4d2fc849ebf8f06
Alexander Ryabets <naeptunao@gmail.com>
2022-08-16 00:57:53 +0200

    aoc6 part 2

--- aoc6/src/main.rs
+++ aoc6/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
     str::FromStr,
@@ -14,26 +15,99 @@ fn read_input() -> Result<Vec<i8>, anyhow::Error> {
     Ok(res?)
 }
 
-fn process_fishes(fishes: &mut Vec<i8>) {
-    let new_fishes = &mut vec![];
-    for fish in fishes.iter_mut() {
-        if *fish > 0 {
-            *fish -= 1;
-        } else {
-            *fish = 6;
-            new_fishes.push(8);
+fn fishes_by_day(fishes: &Vec<i8>) -> HashMap<i8, i64> {
+    let mut by_days: HashMap<i8, i64> = HashMap::new();
+
+    for fish in fishes {
+        match by_days.get(fish) {
+            Some(f) => by_days.insert(*fish, f + 1),
+            None => by_days.insert(*fish, 1),
+        };
+    }
+
+    by_days
+}
+
+fn process_fishes(fishes: &mut HashMap<i8, i64>) {
+    let mut changes: [i64; 9] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    for fish in 0..=8 {
+        match fishes.get(&fish) {
+            Some(0) => {
+                continue;
+            }
+            None => {
+                fishes.insert(fish, 0);
+            }
+            Some(count) => {
+                let i = fish as usize;
+
+                changes[i] -= *count;
+
+                if fish > 0 {
+                    changes[i - 1] += *count;
+                } else {
+                    changes[6] += *count;
+                    changes[8] += *count;
+                }
+            }
         }
     }
 
-    // much effective to join linked lists but input is so tiny so...
-    fishes.append(new_fishes);
+    for fish in 0..=8 {
+        if let Some(count) = fishes.get(&fish) {
+            fishes.insert(fish, count + changes[fish as usize]);
+        }
+    }
+}
+
+fn calculate_fishes_count(fishes: &Vec<i8>, days: i32) {
+    let mut fishes_by_day = fishes_by_day(&fishes);
+
+    for _ in 0..days {
+        process_fishes(&mut fishes_by_day);
+    }
+
+    println!(
+        "fishes count ({days} days): {}",
+        fishes_by_day.values().sum::<i64>()
+    );
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut fishes = read_input()?;
-    for _ in 0..80 {
-        process_fishes(&mut fishes);
-    }
-    println!("fishes count: {}", fishes.len());
+    let fishes = read_input()?;
+
+    calculate_fishes_count(&fishes, 80);
+    calculate_fishes_count(&fishes, 256);
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_map_from_array(expected: [i64; 9], map: &HashMap<i8, i64>) {
+        for i in 0..expected.len() {
+            assert_eq!(map.get(&(i as i8)), Some(&expected[i]));
+        }
+    }
+
+    #[test]
+    fn test_process_fishes() {
+        let mut fishes = HashMap::new();
+        fishes.insert(1, 1);
+        fishes.insert(2, 1);
+        fishes.insert(3, 2);
+        fishes.insert(4, 1);
+
+        process_fishes(&mut fishes);
+        assert_map_from_array([1, 1, 2, 1, 0, 0, 0, 0, 0], &fishes);
+
+        process_fishes(&mut fishes);
+        assert_map_from_array([1, 2, 1, 0, 0, 0, 1, 0, 1], &fishes);
+
+        process_fishes(&mut fishes);
+        assert_map_from_array([2, 1, 0, 0, 0, 1, 1, 1, 1], &fishes);
+    }
 }
